543154c052daf6949b21c2ffeb6e2d056e36b2a2
JHSiam <iamjhsiam@gmail.com>
2025-01-24 23:07:26 +0600

    update and delete added

--- src/dashboard/teacher/MyClass.jsx
+++ src/dashboard/teacher/MyClass.jsx
@@ -17,7 +17,7 @@ export default function MyClass() {
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {myClasses.map((classItem) => (
-            <ClassCard key={classItem._id} classItem={classItem} />
+            <ClassCard key={classItem._id} classItem={classItem} refetch = {refetch}/>
           ))}
         </div>
       )}
